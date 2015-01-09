bac3fb5175f1798d25eaaadfb9a0ff9fbb83c833
Cesar Salmeron <cesarsalmeron1990@gmail.com>
2015-01-09 14:03:36 -0800

    On Fourth Milestone

--- public_html/script.js
+++ public_html/script.js
@@ -5,9 +5,22 @@
  */
 
 
-$("document").ready(function(){
-   $(".practice").css("background-color", "yellow");
-    
+$("document").ready(function() {
+    $(".practice").css("background-color", "yellow");
+
     $('p:last').css({"background-color": "blue", "color": "white"});
-    
+
+    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+
+    $('h1').bind('click', mouseClick);
 });
+
+function mouseOverMe() {
+    $("p").html("Hey");
+}
+function mouseOutMe() {
+    $('h1').html('Hello');
+}
+function mouseClick() {
+    $('p').html('Hello people!!!!!!!!');
+}
